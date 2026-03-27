2c8e9c82801fc33d85b6f590396fb6f053170f36
soeumseang2005-ux <soeumseang2005@gmail.com>
2026-03-27 14:42:35 +0700

    update

--- Algorithm.java
+++ Algorithm.java
@@ -1,4 +1,31 @@
 public class Algorithm {
 
-    
+    public class Algorithm_V1 {
+
+        // Test1 
+        int[] sample = {};
+        int[] input;
+        int[] output;
+        // Test2
+    Algorithm_V1(int[] input){
+        }
+
+    // Test3
+    int[] sum(int[] input) {
+    }
+
+    // Test4
+    int max(int[] input) {
+        return 0;
+
+    }
+
+    // Test5
+    int min(int[] input) {
+        return 0;
+
+    }
+}
+
+
 }
